1ec5f5eecf3b48aee2e0ee99df755b3a8dd00428
PitchayaninM <pitchayanin.makoon@gmial.com>
2023-07-11 19:25:59 +0700

    update

---
+++ Lecture-02 Data types Condition Function/Ex-04.js
@@ -0,0 +1,11 @@
+function dividesEvenly(n1, n2){
+    if (n1 % n2 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(dividesEvenly(98, 7));
+console.log(dividesEvenly(85, 4));
