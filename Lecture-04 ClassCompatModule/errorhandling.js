be5f0bc2546ed88099a0618428cc2b66d848eaf2
PitchayaninM <pitchayaninmakoon@gmail.com>
2023-07-25 09:35:19 +0700

    update

--- Lecture-04 ClassCompatModule/errorhandling.js
+++ Lecture-04 ClassCompatModule/errorhandling.js
@@ -7,12 +7,14 @@ function criticalCode() {
 
 //Try..Catch
 console.log("\n********Try..Catch********\n");
+
 try {
     criticalCode();
 } catch (ex) {
     console.log("Got an error");
     logError(ex);
 }
+
 //Throwing in Try..Catch
 console.log("\n********Throwing in Try..Catch********\n")
 try {
@@ -21,6 +23,7 @@ try {
     console.log("Got an error");
     logError(ex);
 }
+
  //Try..Catch.. Finally
  console.log("\n********Try..Catch..Finally********\n");
  
